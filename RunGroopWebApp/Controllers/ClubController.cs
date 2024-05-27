bda71a433f01c7a7c3fa49fc8ad691e1597374f9
nida esmer ertuğral <esmerertugral@gmail.com>
2024-05-27 11:54:15 +0300

    #

--- RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/Controllers/ClubController.cs
@@ -1,21 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RunGroopWebApp.Data;
 using RunGroopWebApp.Models;
 
 namespace RunGroopWebApp.Controllers
 {
+                  //home
     public class ClubController : Controller
     {
         private readonly ApplicationDbContext _context;
 
+        //private kısmının cıkması ıcın contextte cıkan kutuyo  2.create ile ilerlemeliyiz
         public ClubController(ApplicationDbContext context)
         {
             _context = context;
         }
+        //buraların amacı veritabanından verileri çekmek için geçerli 
         public IActionResult Index()  // controller
         {
             List<Club> clubs = _context.Clubs.ToList(); //model
             return View(clubs); //view
+        }
+        //detail sayfası olusturmak ıcın :
+                             //index    //id
+        public IActionResult Detail(int id)
+        {
+            Club club = _context.Clubs.Include(a=>a.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
         }
     }
 }

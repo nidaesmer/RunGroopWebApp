867dfd317e5b2fc66dad63c1df83de8102dbd02a
nida esmer ertuğral <esmerertugral@gmail.com>
2024-05-29 15:35:23 +0300

    register

---
+++ RunGroopWebApp/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroopWebApp.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Display(Name="Email Address")]
+        [Required(ErrorMessage ="Email address is required")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Password don't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
